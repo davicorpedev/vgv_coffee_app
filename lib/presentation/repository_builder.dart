c108a49e16b4bd641ce288dae89ca6fe8418245c
davicorpedev <davicorpedev@gmail.com>
2022-10-29 20:57:41 +0200

    Coffee Presentation Layer (Basic UI)

---
+++ lib/presentation/repository_builder.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:provider/provider.dart';
+import 'package:vgv_coffee_app/data/datasources/coffee_data_source.dart';
+import 'package:vgv_coffee_app/domain/core/utils/network_info.dart';
+import 'package:vgv_coffee_app/domain/repositories/coffee_repository.dart';
+
+class RepositoryBuilder extends StatelessWidget {
+  final Widget child;
+
+  const RepositoryBuilder({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        RepositoryProvider<CoffeeRepository>(
+          create: (context) => CoffeeRepositoryImpl(
+            dataSource: RepositoryProvider.of<CoffeeDataSource>(context),
+            networkInfo: NetworkInfo(
+              connectionChecker: InternetConnectionChecker(),
+            ),
+          ),
+        ),
+      ],
+      child: child,
+    );
+  }
+}
