c8a0148f073a9cd1b417279adfc1283bfbf98411
davicorpedev <davicorpedev@gmail.com>
2023-06-06 19:05:34 +0200

    Theme Refactor

--- lib/presentation/style/app_themes.dart
+++ lib/presentation/style/app_themes.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
-class AppThemes {
-  static final appTheme = ThemeData(
-    primaryColor: Colors.white,
-    colorScheme: ColorScheme.fromSwatch().copyWith(
-      primary: Colors.white,
-      secondary: Colors.black,
-    ),
-    textTheme: Typography().white,
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(
-        foregroundColor: const Color.fromARGB(255, 39, 35, 35),
-      ),
-    ),
-    outlinedButtonTheme: OutlinedButtonThemeData(
-      style: OutlinedButton.styleFrom(
-        foregroundColor: Colors.white,
-      ),
-    ),
-  );
+abstract class AppTheme {
+  late final Color primary;
+  late final Color secondary;
+  late final TextTheme textTheme;
+  late final Brightness brightness;
+}
+
+class LightTheme implements AppTheme {
+  @override
+  Color primary = Colors.white;
+
+  @override
+  Color secondary = Colors.black;
+
+  @override
+  TextTheme textTheme = Typography().white;
+
+  @override
+  Brightness brightness = Brightness.light;
 }
